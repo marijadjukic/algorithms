a1dfddb3da96a75f1de69552517c92cd5864dcad
Marija Dukic <djukicka.sb@gmail.com>
2020-01-26 00:01:25 +0000

    Implemented radix sort with counting sort as intermediate stable sorting.

--- src/main/java/algorithms/chapter/two/LinearSorting.java
+++ src/main/java/algorithms/chapter/two/LinearSorting.java
@@ -37,4 +37,59 @@ public class LinearSorting {
         return max;
     }
 
+    /**
+     * Procedure assumes that all elements are d-digits
+     * and uses counting sort as intermediate stable sort.
+     * @param array array to sort
+     * @return sorted array
+     */
+    public int[] radixSort(int[] array) {
+        int d = 1; // number of digits
+        int quotient = array[0]/10;
+        //calculates number of digits
+        while(quotient > 0){
+            quotient = quotient / 10;
+            d++;
+        }
+        int n = array.length;
+        for(int i=0; i<d; i++) {
+            array = radixStableSorting(array, i, n);
+        }
+        return array;
+    }
+
+    /**
+     * Returns array sorted on d-digits position using counting sort implementation
+     * @param array array to sort
+     * @param d number of digits
+     * @param n array length
+     * @return
+     */
+    private int[] radixStableSorting(int[] array, int d, int n) {
+        int[] sorted = new int[n];
+        int[] i_digit_arr = new int[n];
+
+        for(int j=0; j<n; j++) {
+            int digit = array[j] % (int)Math.pow(10,d+1);
+            i_digit_arr[j] = digit;
+        }
+
+        int max = findMax(i_digit_arr);
+        int[] counter = new int[max+1];
+
+        for(int j=0; j<n; j++) {
+            counter[i_digit_arr[j]]++;
+        }
+
+        for(int j=1; j<max+1; j++) {
+            counter[j] += counter[j-1];
+        }
+
+        for(int j=n-1; j>=0; j--) {
+            sorted[counter[i_digit_arr[j]]-1] = array[j];
+            counter[i_digit_arr[j]]--;
+        }
+        return sorted;
+    }
+
 }
--- src/test/java/algorithms/chapter/two/LinearSortingTest.java
+++ src/test/java/algorithms/chapter/two/LinearSortingTest.java
@@ -8,6 +8,7 @@ public class LinearSortingTest {
 
     private static final int[] array1 = new int[] {6,0,2,0,1,3,4,6,1,3,2};
     private static final int[] array2 = new int[] {2,5,3,0,2,3,0,3};
+    private static final int[] array3 = new int[] {329,457,657,839,436,720,355};
 
     LinearSorting ls = new LinearSorting();
 
@@ -15,6 +16,12 @@ public class LinearSortingTest {
     public void testCountingSort() {
         assertArrayEquals(new int[] {0,0,1,1,2,2,3,3,4,6,6}, ls.countingSort(array1));
         assertArrayEquals(new int[] {0,0,2,2,3,3,3,5}, ls.countingSort(array2));
+    }
+
+    @Test
+    public void testRadixSort() {
+        assertArrayEquals(new int[] {329,355,436,457,657,720,839}, ls.radixSort(array3));
+
     }
 
 }
