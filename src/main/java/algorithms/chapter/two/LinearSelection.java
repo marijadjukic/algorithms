2438731ec1b08765f3d517d7a37ade2a8864bd0f
Marija Dukic <djukicka.sb@gmail.com>
2020-01-31 22:56:47 +0000

    Implemented randomized selection in linear time based on quick sorting.

---
+++ src/main/java/algorithms/chapter/two/LinearSelection.java
@@ -0,0 +1,46 @@
+package algorithms.chapter.two;
+
+public class LinearSelection {
+
+    public int randomizedSelect(int[] array, int i) {
+        return randomizedSelectRecursive(array, 0, array.length-1, i);
+    }
+
+    private int randomizedSelectRecursive(int[] array, int p, int r, int i) {
+        if(p==r) {
+            return array[p];
+        }
+        int q = randomizedPartition(array,p,r);
+        int k = q - p + 1;
+
+        if (i==k) {
+            return array[q];
+        } else if(i<k) {
+            return randomizedSelectRecursive(array,p,q-1,i);
+        } else {
+            return randomizedSelectRecursive(array,q+1,r,i-k);
+        }
+    }
+
+    private int randomizedPartition(int[] array, int p, int r) {
+        int randomPivot = p + (int) Math.random() * ((r-p) +1);
+        swap(array,randomPivot,r);
+        int pivot = array[r];
+        int i = p-1;
+        for(int j=p; j<r; j++) {
+            if(array[j]<=pivot){
+                i++;
+                swap(array,i,j);
+            }
+        }
+        swap(array,i+1,r);
+        return i+1;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int tmp = array[j];
+        array[j] = array[i];
+        array[i] = tmp;
+    }
+
+}
---
+++ src/test/java/algorithms/chapter/two/LinearSelectionTest.java
@@ -0,0 +1,21 @@
+package algorithms.chapter.two;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class LinearSelectionTest {
+
+    private static final int[] array = new int[]{3,2,9,0,7,5,4,8,6,1};
+
+    private LinearSelection ls = new LinearSelection();
+
+    @Test
+    public void testRandomizedSelect() {
+        assertThat(ls.randomizedSelect(array, 3), is(2));
+        assertThat(ls.randomizedSelect(array,1), is(0));
+        assertThat(ls.randomizedSelect(array, 10), is(9));
+    }
+
+}
